37f38d39bc2e4e6fbf31fa1919ee1de8d8af45f0
whyowisp <95976862+whyowisp@users.noreply.github.com>
2023-02-03 15:57:42 +0200

    removed error throw to get pipeline working again

--- app.js
+++ app.js
@@ -11,11 +11,5 @@ app.listen(PORT, () => {
 })
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
-})
-
-app.get('/version', (req, res) => {
-  res.send('v3') // change this string to ensure a new version deployed
 })
